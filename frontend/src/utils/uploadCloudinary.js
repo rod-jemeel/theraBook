a666edcbf39eb1c09273a7a4d10d034241e37e17
rodjemeel <107553657+rod-jemeel@users.noreply.github.com>
2023-11-29 18:00:38 +0800

    updated api creds

--- frontend/src/utils/uploadCloudinary.js
+++ frontend/src/utils/uploadCloudinary.js
@@ -1,11 +1,11 @@
 const uploadImageToCloudinary = async file => {
   const uploadData = new FormData();
   uploadData.append("file", file);
-  uploadData.append("upload_preset", "doctor-app");
-  uploadData.append("cloud_name", "YOUR_CLOUDINARY_CLOUD_NAME");
+  uploadData.append("upload_preset", "therabook_ups");
+  uploadData.append("cloud_name", "dtbfxyico");
 
   const res = await fetch(
-    "https://api.cloudinary.com/v1_1/YOUR_CLOUDINARY_CLOUD_NAME/image/upload",
+    "https://api.cloudinary.com/v1_1/dtbfxyico/image/upload",
     {
       method: "post",
       body: uploadData,
