4290fefbf0303ee08c86af2c6feab5dafff38038
rodjemeel <107553657+rod-jemeel@users.noreply.github.com>
2023-12-02 12:27:59 +0800

    Changed BASE_URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://therabookweb.onrender.com";
+export const BASE_URL = "https://therabookweb.onrender.com/api/v1";
 export const token = localStorage.getItem("token");
