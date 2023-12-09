68256293f3f93ac1abb8dcec0519d394dbc8d464
rodjemeel <107553657+rod-jemeel@users.noreply.github.com>
2023-12-09 14:37:40 +0800

    Added chat

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,16 +6,9 @@ import icon01 from "../assets/images/icon01.png";
 import icon02 from "../assets/images/icon02.png";
 import icon03 from "../assets/images/icon03.png";
 import About from "../components/About/About.jsx";
-import faqImg from "../assets/images/faq-img.png";
 import { BsArrowRight } from "react-icons/bs";
 
-import ServicesList from "../components/Services/ServicesList";
-import featureImg from "../assets/images/feature-img.png";
-import videoIcon from "../assets/images/video-icon.png";
-import avatarIcon from "../assets/images/avatar-icon.png";
-import DoctorsList from "../components/Doctors/DoctorsList";
-import Testimonial from "../components/Testimonial/Testimonial";
-import FaqList from "../components/Faq/FaqList";
+
 
 const Home = () => {
   return (
--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -1,18 +1,52 @@
-import { services } from "../assets/data/services";
-import ServiceCard from "../components/Services/ServiceCard";
+import { useCallback } from 'react';
+import Talk from 'talkjs';
+import { Session, Chatbox } from '@talkjs/react';
+import { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
 
-const Services = () => {
-  return (
-    <section>
-      <div className="container">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] flex-wrap ">
-          {services.map((item, index) => (
-            <ServiceCard item={item} index={index} key={index} />
-          ))}
-        </div>
-      </div>
-    </section>
+const { state } = useContext(AuthContext);
+const { user } = state;
+
+function ChatComponent() {
+  const syncUser = useCallback(
+    () =>
+      new Talk.User({
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        photoUrl: user.photo,
+        welcomeMessage: 'Hi!',
+        role: user.role,
+      }),
+    [user]
   );
-};
 
-export default Services;
+  const syncConversation = useCallback((session) => {
+    // JavaScript SDK code here
+    const conversation = session.getOrCreateConversation('welcome');
+
+    const other = new Talk.User({
+      id: 'frank',
+      name: 'Frank',
+      email: 'frank@example.com',
+      photoUrl: 'https://talkjs.com/new-web/avatar-8.jpg',
+      welcomeMessage: 'Hey, how can I help?',
+      role: 'default',
+    });
+    conversation.setParticipant(session.me);
+    conversation.setParticipant(other);
+
+    return conversation;
+  }, []);
+
+  return (
+    <Session appId="<APP_ID>" syncUser={syncUser}>
+      <Chatbox
+        syncConversation={syncConversation}
+        style={{ width: '100%', height: '500px' }}
+      ></Chatbox>
+    </Session>
+  );
+}
+
+export default ChatComponent;
