ec845307d56f1ae720635b32c656ff9e5418f28e
rodjemeel <107553657+rod-jemeel@users.noreply.github.com>
2023-12-14 00:08:32 +0800

    Fixed colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,11 +4,11 @@ export default {
   theme: {
     extend: {
       colors: {
-        buttonBgColor: "#0067FF",
+        buttonBgColor: "#9205C4",
         yellowColor: "#FEB60D",
         purpleColor: "#9771FF",
         irisBlueColor: "#01B5C5",
-        headingColor: "#181A1E",
+        headingColor: "#1C1C1C",
         textColor: "#4E545F",
       },
 
