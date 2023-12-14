fc2ae75820926d54220356bbdea6c24bb31bf87c
rodjemeel <107553657+rod-jemeel@users.noreply.github.com>
2023-12-14 11:10:48 +0800

    checkout sucess fix

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -16,7 +16,7 @@ export const getCheckoutSession = async (req, res) => {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       mode: "payment",
-      success_url: `${process.env.CLIENT_SITE_URL}/checkout-success`,
+      success_url: `https://therabookappointment.onrender.com/checkout-success`,
       cancel_url: `${req.protocol}://${req.get("host")}/therapists`,
       customer_email: user.email,
       client_reference_id: req.params.doctorId,
